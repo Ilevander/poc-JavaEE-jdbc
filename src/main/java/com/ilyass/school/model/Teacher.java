1f5130294aaa0178290744ca4dc2a09966ed948f
dell <dell@DESKTOP-4P8U8AR>
2024-04-10 05:22:36 +0000

    Teacher Model

--- src/main/java/com/ilyass/school/model/Teacher.java
+++ src/main/java/com/ilyass/school/model/Teacher.java
@@ -1,5 +1,46 @@
 package com.ilyass.school.model;
 
-public class Teacher {
+import java.io.Serializable;
 
+public class Teacher implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String subject;
+	private String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
